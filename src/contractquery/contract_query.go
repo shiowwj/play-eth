d8454e62a1fda9eabe7f36cd47c399955a58f840
shiowwj <shiowweijun@gmail.com>
2021-12-21 02:59:45 +0800

    some set up for querying contract

---
+++ src/contractquery/contract_query.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/common"
+	ethClientHelper "github.com/shiowwj/server_m1/pkg/eth_helper"
+)
+
+func main() {
+	ec := ethClientHelper.EthClient{}
+	err := ec.InitEthClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// ethClient, err := ethClientHelper.InitEthClient()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	ethClient := ec.EthClient
+	cAddress := common.HexToAddress("0x999e88075692bCeE3dBC07e7E64cD32f39A1D3ab")
+	bc, err := ethClient.CodeAt(context.Background(), cAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	isContract := len(bc) > 0
+	fmt.Printf("is contract: %v\n", isContract)
+}
